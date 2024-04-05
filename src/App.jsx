54667316b54ee3310001e097e45eeb306b949dfd
hieple <hieplevuc@gmail.com>
2024-04-05 15:41:50 +0700

    feat: setup theme with mui

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ function App() {
   return (
     <>
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined" sx={{ color: 'red' }} >Outlined</Button>
+      <Button variant="contained" color='secondary' >Contained</Button>
+      <Button variant="outlined" color='primary' >Outlined</Button>
     </>
   )
 }
