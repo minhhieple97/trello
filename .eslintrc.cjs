664f2aabfd36f29eb767d9c8856d5d3f0c61fb04
hieple <hieplevuc@gmail.com>
2024-04-05 14:35:28 +0700

    feat: add eslint rule for import component from MUI

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,6 +36,12 @@ module.exports = {
     'keyword-spacing': 1,
     'comma-dangle': 1,
     'comma-spacing': 1,
-    'arrow-spacing': 1
+    'arrow-spacing': 1,
+    'no-restricted-imports': [
+      'error',
+      {
+        'patterns': ['@mui/*/*/*']
+      }
+    ]
   }
 }
